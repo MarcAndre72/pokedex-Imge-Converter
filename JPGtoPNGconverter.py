70e5d1c68fac5041b9d6818aecd06cc6cef45d62
MarcAndre72 <marcandrelamarche@yahoo.com>
2024-11-07 10:06:55 +0000

    initial commit: JPG to PNG converter

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from PIL import Image
+from PIL import Image, ImageFilter # Added ImageFilter
 
 path = sys.argv[1]
 directory = sys.argv[2]
@@ -8,11 +8,14 @@ directory = sys.argv[2]
 if not os.path.exists(directory):
     os.makedirs(directory)
     
-
 for filename in os.listdir(path):
   clean_name = os.path.splitext(filename)[0]
   img = Image.open(f'{path}{filename}')
   #added the / in case user doesn't enter it. You may want to check for this and add or remover it. 
+  
+  # Add blur effect for Charmander
+  if clean_name.lower() == 'charmander':
+      img = img.filter(ImageFilter.BLUR)
+      print('Charmander image has been blurred!')
   img.save(f'{directory}/{clean_name}.png', 'png')
-  print('all done!')
-
+print('All images have been converted successfully!')
